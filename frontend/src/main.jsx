2b9fac16477d79be9fa039f1b5efe478d946dc97
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-10 16:15:36 +0530

    Load final CSS audit layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,6 +21,7 @@ import "./styles/layout-mobile-polish.css";
 import "./styles/product-experience.css";
 import "./styles/scan-theme.css";
 import "./styles/obsidian-contrast-fix.css";
+import "./styles/css-audit.css";
 import "./lib/localBarcodeDetector";
 import { applyTheme, getTheme } from "./lib/theme";
 import { LanguageProvider } from "./components/LanguageProvider";
