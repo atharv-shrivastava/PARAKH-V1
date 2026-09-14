3b027b0f007656a29d33893f3a563b56ed258bd4
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-14 21:01:01 +0530

    chore: remove unused dashboard refresh helper

--- frontend/src/lib/dashboardRefresh.js
+++
@@ -1,6 +0,0 @@
-export const DASHBOARD_DATA_UPDATED_EVENT = "parakh:data-updated";
-
-export function announceDashboardDataUpdated(detail = {}) {
-  if (typeof window === "undefined") return;
-  window.dispatchEvent(new CustomEvent(DASHBOARD_DATA_UPDATED_EVENT, { detail }));
-}
