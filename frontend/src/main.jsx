b64909a1bfe4880da1c8c944e3637642906a3819
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-14 13:42:40 +0530

    fix: scope unable-to-verify review panel (#12)
    * fix: scope unable-to-verify review to scan page
    
    * fix: constrain unable-to-verify review width
    
    * fix: scope unable-to-verify review to scan page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import AutoTranslate from "./components/AutoTranslate";
-import UnableToVerifyReview from "./components/UnableToVerifyReview";
+import ScanScopedUnableToVerifyReview from "./components/ScanScopedUnableToVerifyReview";
 import StartupSplash from "./components/StartupSplash";
 import "./styles/global.css";
 import "./styles/components.css";
@@ -31,7 +31,7 @@ createRoot(document.getElementById("root")).render(
       <StartupSplash>
         <AutoTranslate />
         <App />
-        <UnableToVerifyReview />
+        <ScanScopedUnableToVerifyReview />
       </StartupSplash>
     </LanguageProvider>
   </StrictMode>
